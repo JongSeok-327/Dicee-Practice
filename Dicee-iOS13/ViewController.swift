3a6d9e6200606ad6600899bc9653a53e09a9861a
JongSeok-327 <121272922+JongSeok-327@users.noreply.github.com>
2023-01-19 01:56:12 +0900

    Simple Dice Roll
    - add Simple Dice Roll Event.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,13 +9,25 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    let dices = [UIImage(named: "DiceOne"),UIImage(named: "DiceTwo"),UIImage(named: "DiceThree"),UIImage(named: "DiceFour"),UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = UIImage(named: "DiceSix")
-        diceImageView1.image = UIImage(named: "DiceTwo")
+        diceImageView1.image = dices[0]
+        diceImageView2.image = dices[5]
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = dices[leftDiceNumber % 6]
+        diceImageView2.image = dices[rightDiceNumber % 6]
+        
+        leftDiceNumber += 1
+        rightDiceNumber += 1
+    }
+    
 }
 
